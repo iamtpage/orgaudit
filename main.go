aa6f3bcad6f7566be6dbfc76db9c293a6471809b
Tyler Page <iamtpage@users.noreply.github.com>
2018-05-17 11:44:23 -0700

    added bl/wl support

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/parnurzeal/gorequest"
 	"github.com/pivotalservices/cf-mgmt/cloudcontroller"
@@ -24,18 +25,35 @@ var (
 	sysDomain    = kingpin.Flag("system-domain", "CF system domain").Short('d').Required().String()
 	userID       = kingpin.Flag("user-id", "user ID").Envar("USER_ID").Required().String()
 	clientSecret = kingpin.Flag("client-secret", "client secret").Envar("CLIENT_SECRET").Required().String()
-	org          = kingpin.Flag("org", "the name of the org").Short('o').Required().String()
+	whitelist    = kingpin.Flag("whitelist", "comma-separated whitelist of orgs to include").Short('w').Required().String()
+	blacklist    = kingpin.Flag("blacklist", "comma-separated blacklist of orgs to ignore").Short('b').Required().String()
 )
 
 func main() {
 	kingpin.Parse()
+
+	//parse whitelist/blacklist
+	wslice := strings.Split(*whitelist, ",")
+	bslice := strings.Split(*blacklist, ",")
+
+	//convert to map for more efficient checking with large bl/wl
+	bmap := make(map[string]bool)
+	wmap := make(map[string]bool)
+
+	for _, s := range wslice {
+		wmap[s] = true
+	}
+
+	for _, s := range bslice {
+		bmap[s] = true
+	}
 
 	token, err := getToken("https://uaa."+*sysDomain, *userID, *clientSecret)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	cc := cloudcontroller.NewManager("https://api."+*sysDomain, token)
+	cc := cloudcontroller.NewManager("https://api."+*sysDomain, token, false)
 
 	orgs, err := cc.ListOrgs()
 	if err != nil {
@@ -43,40 +61,46 @@ func main() {
 	}
 
 	var orgGUID string
+	var orgNAME string
 	for _, o := range orgs {
-		if o.Entity.Name == *org {
-			orgGUID = o.MetaData.GUID
-			break
+
+		if bmap[o.Entity.Name] {
+			//Org is in BL, skip it
+			continue
 		}
-	}
 
-	if orgGUID == "" {
-		log.Fatal("cannot find org", *org)
-	}
+		if wmap[o.Entity.Name] {
+			//Org is in WL, do the thing
+			orgGUID = o.MetaData.GUID
+			orgNAME = o.Entity.Name
 
-	spaces, err := cc.ListSpaces(orgGUID)
-	if err != nil {
-		log.Fatalf("cannot list spaces for org %s: %v", *org, err)
-	}
+			spaces, err := cc.ListSpaces(orgGUID)
 
-	users, err := cc.GetCFUsers(orgGUID, "organizations", "auditors")
-	if err != nil {
-		log.Fatalf("cannot find org auditors for %s: %v", *org, err)
-	}
-
-	for user := range users {
-		for _, space := range spaces {
-			log.Println("assigning SpaceAuditor to", user, "for space", space.Entity.Name)
-			err = cc.AddUserToSpaceRole(user, "auditors", space.MetaData.GUID)
 			if err != nil {
-				log.Fatalf("error assigning SpaceAuditor to user %s: %v", user, err)
+				log.Fatalf("cannot list spaces for org %s: %v", orgNAME, err)
+			}
+
+			users, err := cc.GetCFUsers(orgGUID, "organizations", "auditors")
+
+			if err != nil {
+				log.Fatalf("cannot find org auditors for %s: %v", orgNAME, err)
+			}
+
+			for user := range users {
+				for _, space := range spaces {
+					log.Println("assigning SpaceAuditor to", user, "for space", space.Entity.Name)
+					err = cc.AddUserToSpaceRole(user, "auditors", space.MetaData.GUID)
+					if err != nil {
+						log.Fatalf("error assigning SpaceAuditor to user %s: %v", user, err)
+					}
+				}
 			}
 		}
 	}
 }
 
 func getToken(uaaURL, user, secret string) (string, error) {
-	
+
 	request := gorequest.New()
 	request.TargetType = "form"
 
